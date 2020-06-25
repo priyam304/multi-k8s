0810fb3e94f46eabf02ca215c4615be2acb00f47
priyam304 <priyam.vaidya@sjsu.edu>
2020-06-24 18:36:38 -0700

    added deployment_script.sh and deploment in travis

---
+++ deployment_script.sh
@@ -0,0 +1,19 @@
+
+docker build -t priyam304/multi-client:latest -t priyam304/multi-client:$SHA ./client 
+docker build -t priyam304/multi-server:latest -t priyam304/multi-server:$SHA ./server 
+docker build -t priyam304/multi-worker:latest -t priyam304/multi-worker:$SHA ./worker 
+
+docker push priyam304/multi-client:latest
+docker push priyam304/multi-server:latest
+docker push priyam304/multi-worker:latest
+
+docker push priyam304/multi-client:$SHA
+docker push priyam304/multi-server:$SHA
+docker push priyam304/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployment/client-deployment client=stephengrider/multi-client
+kubectl set image deployment/server-deployment server=cygnetops/multi-server-pgfix-5-11
+kubectl set image deployment/worker-deployment worker=stephengrider/multi-worker
+
