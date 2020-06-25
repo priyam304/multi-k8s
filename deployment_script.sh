615cf9b26947464f36d379091ce7ac602ef89463
priyam304 <priyam.vaidya@sjsu.edu>
2020-06-24 19:07:30 -0700

    image changed in deployment_script

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,7 +1,7 @@
 
-docker build -t priyam304/multi-client:latest -t priyam304/multi-client:$SHA ./client 
-docker build -t priyam304/multi-server:latest -t priyam304/multi-server:$SHA ./server 
-docker build -t priyam304/multi-worker:latest -t priyam304/multi-worker:$SHA ./worker 
+docker build -t priyam304/multi-client:latest -t priyam304/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t priyam304/multi-server:latest -t priyam304/multi-server:$SHA -f ./server/Dockerfile ./server 
+docker build -t priyam304/multi-worker:latest -t priyam304/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
 
 docker push priyam304/multi-client:latest
 docker push priyam304/multi-server:latest
@@ -13,7 +13,7 @@ docker push priyam304/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployment/client-deployment client=stephengrider/multi-client
-kubectl set image deployment/server-deployment server=cygnetops/multi-server-pgfix-5-11
-kubectl set image deployment/worker-deployment worker=stephengrider/multi-worker
+kubectl set image deployment/client-deployment client=priyam304/multi-client:$SHA
+kubectl set image deployment/server-deployment server=priyam304/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=priyam304/multi-worker:$SHA
 
