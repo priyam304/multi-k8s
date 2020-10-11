9e45354c9411476e79091a66f286386a25e96ff8
Priyam Vaidya <priyam.vaidya@sjsu.edu>
2020-10-11 16:49:51 -0700

    Update deployment_script.sh

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,15 +1,15 @@
 
-docker build -t priyam304/multi-client:latest -t priyam304/multi-client:$SHA -f ./client/Dockerfile ./client 
-docker build -t priyam304/multi-server:latest -t priyam304/multi-server:$SHA -f ./server/Dockerfile ./server 
-docker build -t priyam304/multi-worker:latest -t priyam304/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
+docker build -t priyam304/multi-client:latest -t priyam304/multi-client:$GIT_COMMIT -f ./client/Dockerfile ./client 
+docker build -t priyam304/multi-server:latest -t priyam304/multi-server:$GIT_COMMIT -f ./server/Dockerfile ./server 
+docker build -t priyam304/multi-worker:latest -t priyam304/multi-worker:$GIT_COMMIT -f ./worker/Dockerfile ./worker 
 
 docker push priyam304/multi-client:latest
 docker push priyam304/multi-server:latest
 docker push priyam304/multi-worker:latest
 
-docker push priyam304/multi-client:$SHA
-docker push priyam304/multi-server:$SHA
-docker push priyam304/multi-worker:$SHA
+docker push priyam304/multi-client:$GIT_COMMIT
+docker push priyam304/multi-server:$GIT_COMMIT
+docker push priyam304/multi-worker:$GIT_COMMIT
 
 kubectl apply -f k8s
 
