28d73afaba3b3b0a41840af5ccf96a1b5914e028
Priyam Vaidya <priyam.vaidya@sjsu.edu>
2020-10-13 22:15:05 -0700

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  default = "the-devops-project-cluster"
+}
+
+variable "project" {
+  default = "the-devops-project-06152020"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
+
