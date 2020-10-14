01cb360cb521a268662914f451e071a705834f29
Priyam Vaidya <priyam.vaidya@sjsu.edu>
2020-10-13 22:22:02 -0700

    Update deployment_script.sh

--- deployment_script.sh
+++ deployment_script.sh
@@ -13,7 +13,7 @@ docker push priyam304/multi-worker:$GIT_COMMIT
 
 kubectl apply -f k8s
 
-kubectl set image deployment/client-deployment client=stephengrider/multi-client
-kubectl set image deployment/server-deployment server=cygnetops/multi-server-pgfix-5-11
-kubectl set image deployment/worker-deployment worker=stephengrider/multi-worker
+kubectl set image deployment/client-deployment client=priyam304/multi-client:$GIT_COMMIT
+kubectl set image deployment/server-deployment server=priyam304/multi-server:$GIT_COMMIT
+kubectl set image deployment/worker-deployment worker=priyam304/multi-worker:$GIT_COMMIT
 
